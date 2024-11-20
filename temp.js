9564923b9527989e2f8e07598bf455c78b9d473f
Leelakrishna K <kleelakrishna753@gmail.com>
2024-11-20 17:50:55 +0530

    sticky

---
+++ temp.js
@@ -0,0 +1,16 @@
+db.products.insertMany(
+    [
+        {
+            "name":"Product 5",
+            "desc":"This is the description of product 3",
+            "price":95,
+            "url":"https://picsum.photos/id/23/3887/4899"
+        },
+        {
+            "name":"Product 6",
+            "desc":"This is the description of product 4",
+            "price":96,
+            "url":"https://picsum.photos/id/23/3887/4899"
+        }
+    ]
+);
